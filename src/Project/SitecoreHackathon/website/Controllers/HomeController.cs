2d9cb4f779851af8f535b57ae4fc1acc50550ac1
Pavel Ernesto Navarro Guerrero <pnavarro@oshyn.com>
2020-02-28 22:13:55 -0500

    First Commit

---
+++ src/Project/SitecoreHackathon/website/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Sitecore.Mvc.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Oshyn.BitsPls.Project.HackathonSite.Controllers
+{
+    public class HomeController : SitecoreController
+    {
+        public override ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
